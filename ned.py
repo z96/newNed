35a29eb154256e7ae6f80f70495b273f89622f1c
z96 <zcorse@slu.edu>
2015-03-20 20:20:07 -0500

    Refactoring into main()
    Runs firstCheck(), then checks to see if that is true/false. if false,
    retries firstCheck(), if true, redefines comic date with
    redefineComicDate()

--- ned.py
+++ ned.py
@@ -15,32 +15,59 @@ FirstComicDate = 'March 10th, 2015'
 checkForNewDate = False
 
 TEST = 'string test'
-
+firstNewComicCheckTrue = False
 
 ##If there is a new comic, 
 ##update the variable that is looked at to see if a new comic has been uploaded
-def redefineComicDate(check):
-    if (check == True):
-        arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
-                                                        #Regular expression to find a date in the format of Month Day(th), Year
-        newComicDate = arrayOfDates[0]                  #The first date found by the regular expression is set to the new comic date
-        print("This comic was posted on: " +newComicDate)
-        #return newComicDate
+
+
+def redefineComicDate():
+    #if (check == True):
+    arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
+    #Regular expression to find a date in the format of Month Day(th), Year
+    newComicDate = arrayOfDates[0]                  #The first date found by the regular expression is set to the new comic date
+    print("This comic was posted on: " +newComicDate)
+    #return newComicDate
+
+
 
 
 def firstCheck():
-	if FirstComicDate in decoded:
-		print("There is no new comic")
-	else:
-		print("There is a new Nedroid comic!")
-		checkForNewDate = True
-		redefineComicDate(checkForNewDate)			#this is where redefineComicDate happens
+    if FirstComicDate in decoded:
+        print("There is no new comic")
+        comicCheck = False
+        return noNewComic
 
+        
+    else:
+        print("There is a new Nedroid comic!")
+        comicCheck = True                 #move away from "FirstComicDate"
+        return comicCheck
+        
+
+
+
+#redefineComicDate(checkForNewDate)
+#this is where redefineComicDate happens
+        
+
+
+def continuousCheck(date):
+    input()
+
+    #every so often, check for new comic
+    
 
 
 def main():
+    foundNewComic = firstCheck()
+    
+    if (foundNewComic == False):
+        foundNewComic = firstCheck()
+    elif (foundNewComic == True):
+        redefineComicDate()
 
-    firstCheck()
+
     x = 0
     while (x is not 1):
         input('Hit enter to exit program')
