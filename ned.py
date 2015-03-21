4eae520a2901495231b11255b65794413a592084
z96 <zcorse@slu.edu>
2015-03-20 19:39:07 -0500

    Organization
    Organized variables

--- ned.py
+++ ned.py
@@ -5,22 +5,27 @@ import urllib.parse
 import re
 
 URL = 'http://nedroid.com/'
+FirstComicDate = 'March 10th, 2015'
+
 request = urllib.request.urlopen(URL)
+puretext = request.read()			#puretext is of type 'byte'
+decoded = puretext.decode("utf-8") 	#this changes it from 'byte' to 'str' format so that it can be worked with
+
+FirstComicDate = 'March 10th, 2015'
+checkForNewDate = False
+
 TEST = 'string test'
 
 
-#If there is a new comic, 
-#update the variable that is looked at to see if a new comic has been uploaded
-
+##If there is a new comic, 
+##update the variable that is looked at to see if a new comic has been uploaded
 def redefineComicDate(check):
     if (check == True):
         arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
-        #Regular expression to find a date in the format of Month Day(th), Year
-        newComicDate = arrayOfDates[0] #The first date found by the regular expression is set to the new comic date
+                                                        #Regular expression to find a date in the format of Month Day(th), Year
+        newComicDate = arrayOfDates[0]                  #The first date found by the regular expression is set to the new comic date
         print("The new 'comic date' is: " +newComicDate)
         #return newComicDate
-        
-        
 
 
 def firstCheck():
@@ -33,15 +38,11 @@ def firstCheck():
 
 
 
-puretext = request.read()			#puretext is of type 'byte'
-decoded = puretext.decode("utf-8") 	#this changes it from 'byte' to 'str' format so that it can be worked with
-FirstComicDate = 'March 10th, 2015'
-checkForNewDate = False
 
-		
-		
+
+
 firstCheck()
-#print(newComicDate)
+
 	
 	
 x = 0
@@ -62,7 +63,6 @@ def main():
 		break
 
 main()
-
 
 
 
