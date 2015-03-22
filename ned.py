01cd14cd3a958628f08c5d216c59682370daa892
z96 <zcorse@slu.edu>
2015-03-21 20:10:38 -0500

    continuousCheck() implemented
    took out "if, elif, else" code from main function and refactored it into
    its own function that is set to run continuously in main

--- ned.py
+++ ned.py
@@ -14,20 +14,13 @@ request = urllib.request.urlopen(URL)
 puretext = request.read()			#puretext is of type 'byte'
 decoded = puretext.decode("utf-8") 	#this changes it from 'byte' to 'str' format so that it can be worked with
 
-FirstComicDate = 'March 10th, 2015'
-checkForNewDate = False
-
-TEST = 'string test'
-firstNewComicCheckTrue = False
-
 
 #s = sched.scheduler(time.time, time.sleep)
-
-
-
-
 #s.enter(10, 1, timer, (s,))
 #s.run()
+
+
+
 
 ##If there is a new comic, 
 ##update the variable that is looked at to see if a new comic has been uploaded
@@ -36,6 +29,7 @@ def redefineComicDate():
     arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
     #Regular expression to find a date in the format of Month Day(th), Year
     newComicDate = arrayOfDates[0]                  #The first date found by the regular expression is set to the new comic date
+
     print("This comic was posted on: " +newComicDate)
     #return newComicDate
 
@@ -46,7 +40,7 @@ def firstCheck():
     if FirstComicDate in decoded:
         print("There is no new comic")
         comicCheck = False
-        return noNewComic
+        return comicCheck
 
         
     else:
@@ -58,25 +52,40 @@ def firstCheck():
         
 
 
-def continuousCheck(date):
-    input()
-
+def continuousCheck(newComicGate):
+    #print("")
+    checker = newComicGate
     #every so often, check for new comic
+
+    if (checker == False):
+        foundNewComic = firstCheck()
+    elif(checker == True):
+        redefineComicDate()
+    else:
+        print("There is an error in the code. SHUTTING DOWN.")
+        sys.exit()
+
+    #time.sleep(5) #for testing purposes
+    #continuousCheck(checker)
     
 
 
 def main():
     foundNewComic = firstCheck()
-    
-
-
 
     ##put this in function!
+    #if (foundNewComic == False):
+    #    foundNewComic = firstCheck()
+    #elif (foundNewComic == True):
+    #    redefineComicDate()
 
-    if (foundNewComic == False):
-        foundNewComic = firstCheck()
-    elif (foundNewComic == True):
-        redefineComicDate()
+    #time.sleep(5)
+
+    runContinuousCheck = True
+    while (runContinuousCheck == True):
+        continuousCheck(foundNewComic)
+
+
 
    #timer()
    #s.enter(10, 1, timer, (s,))
@@ -88,6 +97,9 @@ def main():
         input('Hit enter to exit program')
         print("Shutting down....")
         x = 1
+
+
+
 
 #def timer(sc): 
 #    print("Doing stuff...")
