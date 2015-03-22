951b935a71ba0dbc5a39d44f00da3b24274a44bf
z96 <zcorse@slu.edu>
2015-03-22 03:20:23 -0500

    Correct function of check() underway
    ugh

--- ned.py
+++ ned.py
@@ -13,7 +13,7 @@ ComicDate = 'March 10th, 2015'
 request = urllib.request.urlopen(URL)
 puretext = request.read()			#puretext is of type 'byte'
 decoded = puretext.decode("utf-8") 	#this changes it from 'byte' to 'str' format so that it can be worked with
-
+foundNewComic = True
 
 #s = sched.scheduler(time.time, time.sleep)
 #s.enter(10, 1, timer, (s,))
@@ -25,8 +25,6 @@ decoded = puretext.decode("utf-8") 	#this changes it from 'byte' to 'str' format
 ##If there is a new comic, 
 ##update the variable that is looked at to see if a new comic has been uploaded
 def redefineComicDate():
-    #if (check == True):
-    
     arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)\s\d\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
     #Regular expression to find a date in the format of Month Day(th), Year
     
@@ -42,13 +40,13 @@ def firstCheck():
     if ComicDate in decoded:
         print("There is no new comic")
         comicCheck = False
-        return comicCheck
+        return comicCheck #FALSE
 
         
     else:
         print("There is a new Nedroid comic!")
         comicCheck = True                 #move away from "FirstComicDate"
-        return comicCheck
+        return comicCheck #TRUE
         
 
         
@@ -58,52 +56,80 @@ def continuousCheck(newComicGate):
     #print("")
     checker = newComicGate
     #every so often, check for new comic
-
+   # checker = True
     if (checker == False):
-        foundNewComic = check()
+        #foundNewComic = check()
+        #print("using check() in continunousChecker() because checker is false")
+        check()
+        something = False
+        return something
+
     elif(checker == True):
+        print("calling redefineComicDate()")
         redefineComicDate()
+        something = True
+        return something
+
     else:
         print("An unknown error has occured. SHUTTING DOWN.")
         input("Hit enter to exit.")
         sys.exit()
-    
+
+
 
 def check():
+    #print("CHECK IS BEING RUN")
     if ComicDate in decoded:
         comicCheck = False
+        #print("ComicDate is in decoded, found using check(), do not print a new date")
         return comicCheck
     else:
-        print("There is a new Nedroid comic!")
+        print("There is a new Nedroid comic! [in check()]")     #remove [in check()}
         comicCheck = True
         return comicCheck
 
 
+def con():
+    if (foundNewComic == False):
+        print("con(): foundNewComic is False")
+        check()
+        something = False
+        return something
 
+    if (foundNewComic == True):
+        print("con(): foundNewComic is True")
+        redefineComicDate()
+        print("Calling redefineComicDate() in con()")
+        something = True
+        return something
+
+        
 
 
 
 def main():
-    foundNewComic = firstCheck()
+    foundNewComic = firstCheck()  #IF TRUE, A NEW COMIC IS FOUND AND CONTINUOUS CHECK WILL CALL REDEFINE COMIC DATE, IF FALSE, WILL TRY "check()"
     everyHour = 60**2
-
+    #foundNewComic = False
+    
+    
     runContinuousCheck = True
     while (runContinuousCheck == True):
-        continuousCheck(foundNewComic)
+        #foundNewComic = continuousCheck(foundNewComic)
+        foundNewComic = con()
+        time.sleep(5)
 
 
-   #timer()
-   #s.enter(10, 1, timer, (s,))
-   #s.run()
+    #timer()
+    #s.enter(10, 1, timer, (s,))
+    #s.run()
+    #x = 0
+    #while (x is not 1):
+    #    input('Hit enter to exit program')
+    #    print("Shutting down....")
+    #    x = 1
 
-
-    x = 0
-    while (x is not 1):
-        input('Hit enter to exit program')
-        print("Shutting down....")
-        x = 1
-
-
+main()
 
 
 #def timer(sc): 
@@ -116,5 +142,3 @@ def main():
 
 #    sc.enter(4, 1, timer, (sc,))
 #s = sched.scheduler(time.time, time.sleep)
-
-main()
