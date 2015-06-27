505adc0140d277bd06f32ceea73f015d4cb125fd
z96 <zcorse@slu.edu>
2015-06-26 21:23:21 -0500

    Removed test()
    Function used for testing which was refactored.

--- nedroid.py
+++ nedroid.py
@@ -16,23 +16,6 @@ puretext = request.read()
 decoded = puretext.decode("utf-8")
 
 
-
-#def test():
-#    arrayOfDates = re.findall('(?:January|February|March|April|May|June|July|August|September|October|November|December)(?:\s|\d)\d(?:st|nd|rd|th)\,\s\d{4}', decoded)
-#    latestComicDate = 'May 3rd, 2015'
-#    #Regular expression to find a date in the format of Month Day(th), Year 
-#    comicDate = arrayOfDates[0]                  #The first date found by the regular expression is set to the new comic date
-#    print("The latest comic was posted on: " +comicDate)
-
-#    isThereANewComic = False
-
-#    print(isThereANewComic)
-
-#    latestComicDate = comicDate
-#    print("latestComicDate = "+ latestComicDate)
-#    return latestComicDate
-
-
 def newCheck(x,y):
     if x == y:  #if comicDate == latestComicDate
         return False #there is no new comic
